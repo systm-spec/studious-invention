49f2b50f2c7ef80d25748d812dde40104287ed92
Stread <irgendwasmitmedien1@gmail.com>
2024-11-02 16:33:31 +0100

    added fast-quit on escape, layoutet and colored elemnts

--- src/modules/CharacterSelect.py
+++ src/modules/CharacterSelect.py
@@ -19,7 +19,7 @@ class CharacterSelect(ctk.CTkFrame):
         self.heading_lbl.grid(padx= 2, pady= 2, ipadx= 100, ipady= 10)
 
         # Character_Config_Frame
-        self.body_frame= ctk.CTkFrame(self, fg_color= "blue")
+        self.body_frame= ctk.CTkFrame(self, fg_color= "transparent")
         self.body_frame.grid(row= 1, column= 0)
 
         # Body_Grid
@@ -28,12 +28,12 @@ class CharacterSelect(ctk.CTkFrame):
         self.body_frame.grid_columnconfigure(1, weight= 3)
 
         # Character_Config_Frame
-        self.character_config_frame= CharacterConfig(self.body_frame, fg_color= "red")
-        self.character_config_frame.grid( column= 0, ipadx= 6, ipady= 6, sticky="sn", padx=5, pady=4)
+        self.character_config_frame= CharacterConfig(self.body_frame, fg_color= "transparent",border_width=1, border_color="#808080")
+        self.character_config_frame.grid( column= 0, sticky="nwsn", padx=12, pady=4)
 
         # Character_Preview_Frame
-        self.character_preview_frame= CharacterPreview(master=self.body_frame, fg_color= "green")
-        self.character_preview_frame.grid(row= 0, column= 1, padx= 5, pady=4)
+        self.character_preview_frame= CharacterPreview(master=self.body_frame, fg_color= "transparent")
+        self.character_preview_frame.grid(row= 0, column= 1, padx= 0, pady=4)
 
 
 
